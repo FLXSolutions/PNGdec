333bb35b86358529d9ad7b2c1858acc1128282be
Stephen Brawner <brawner@gmail.com>
2024-11-11 12:00:40 -0800

    Only include headers once and include what's used

--- src/inffast.h
+++ src/inffast.h
@@ -1,3 +1,4 @@
+#pragma once
 /* inffast.h -- header to use inffast.c
  * Copyright (C) 1995-2003, 2010 Mark Adler
  * For conditions of distribution and use, see copyright notice in zlib.h
@@ -7,5 +8,9 @@
    part of the implementation of the compression library and is
    subject to change. Applications should only use zlib.h.
  */
+
+#include "inflate.h"
+#include "zconf.h"
+#include "zutil.h"
 
 void ZLIB_INTERNAL inflate_fast OF((z_streamp strm, unsigned start));
